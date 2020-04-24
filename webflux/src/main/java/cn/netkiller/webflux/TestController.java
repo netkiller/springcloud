e799257933b84e5587f43174dae0ff49b4d35498
Neo Chan <netkiller@msn.com>
2020-04-24 22:54:18 +0800

    validation

--- webflux/src/main/java/cn/netkiller/webflux/TestController.java
+++ webflux/src/main/java/cn/netkiller/webflux/TestController.java
@@ -4,9 +4,15 @@ import java.time.Duration;
 import java.util.concurrent.ThreadLocalRandom;
 import java.util.function.Function;
 
+import javax.validation.Valid;
+
 import org.reactivestreams.Publisher;
 import org.springframework.http.codec.ServerSentEvent;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.ObjectError;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -40,5 +46,14 @@ public class TestController {
 			}
 		});
 	}
+
+	// @PostMapping("/adduser")
+	// public Mono<String> addUser(@RequestBody @Valid Mono<User> user, BindingResult bindingResult) {
+	// // 如果有参数校验失败，会将错误信息封装成对象组装在BindingResult里
+	// for (ObjectError error : bindingResult.getAllErrors()) {
+	// return Mono.just(error.getDefaultMessage());
+	// }
+	// return Mono.just("Hello world!");
+	// }
 
 }
